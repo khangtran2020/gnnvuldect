8630096b7c0c178974854d8c835ae750c2f22e94
khangtran2020 <kt36@njit.edu>
2024-12-24 14:12:43 -0500

    debuging

--- data/core.py
+++ data/core.py
@@ -4,7 +4,8 @@ from torch.utils.data import Dataset, DataLoader
 class Data(Dataset):
 
     def __init__(self, name: str) -> None:
-        super().__init__(name=name)
+        # super().__init__(name=name)
+        self.name = name
 
     def process(self):
         pass
