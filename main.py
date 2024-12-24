947e41f9c083587a3bbafcaa452252c259d7f058
khangtran2020 <kt36@njit.edu>
2024-12-24 14:26:02 -0500

    debuging

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def run(args):
     # Load Data
     console.log("Loading dataset: ", args.dataset)
     dataset = get_data(args.dataset)
+    # dataset.
     console.log("Dataset loaded, size: ", len(dataset))
+    console.log("Taking a look at the first element: ", dataset[0])
 
 
 if __name__ == "__main__":
