bd43b8b81f98f913cbbf2f9f310f48ef19f875b9
khangtran2020 <kt36@njit.edu>
2024-12-24 22:00:05 -0500

    debuging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config import parse_args
 from data.utils import get_data
 from utils.console import console
+from torch.utils.data import DataLoader
 
 
 def run(args):
@@ -8,9 +9,17 @@ def run(args):
     # Load Data
     console.log("Loading dataset: ", args.dataset)
     dataset = get_data(args.dataset)
-    # dataset.
     console.log("Dataset loaded, size: ", len(dataset))
     console.log("Taking a look at the first element: ", dataset[0])
+
+    # Data Loader
+    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    console.log("Data Loader created")
+    # Get the first batch
+    for batch in loader:
+        console.log("Batch size: ", len(batch))
+        console.log("Batch: ", batch)
+        break
 
 
 if __name__ == "__main__":
